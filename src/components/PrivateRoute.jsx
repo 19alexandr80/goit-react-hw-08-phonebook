8ef55b3f4b8c34686dcba63679316c6b42055b14
19alexandr80 <usanka1980@gmail.com>
2023-08-11 06:15:20 +0300

    log

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,6 +3,5 @@ import { Navigate } from 'react-router-dom';
 
 export const PrivateRoute = ({ element: Element, redirecrTo = '/' }) => {
   const isLoggedIn = useSelector(state => state.contacts.isLoggedIn);
-  console.log(isLoggedIn);
   return !isLoggedIn ? <Navigate to={redirecrTo} /> : <Element />;
 };
