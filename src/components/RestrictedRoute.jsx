0df9f6973e7840ba2b6dcd6239353579de9ff381
19alexandr80 <usanka1980@gmail.com>
2023-08-11 06:14:50 +0300

    log

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -3,6 +3,5 @@ import { Navigate } from 'react-router-dom';
 
 export const RestrictedRoute = ({ element: Element, redirecrTo = '/' }) => {
   const isLoggedIn = useSelector(state => state.contacts.isLoggedIn);
-  console.log(isLoggedIn);
   return isLoggedIn ? <Navigate to={redirecrTo} /> : <Element />;
 };
